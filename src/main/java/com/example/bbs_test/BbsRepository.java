9cec616945129ee7680eef5afe1d2c941a750741
terappy <terakei818@gmail.com>
2018-02-18 19:16:52 +0900

    make contents of BbsRepository

--- src/main/java/com/example/bbs_test/BbsRepository.java
+++ src/main/java/com/example/bbs_test/BbsRepository.java
@@ -1,5 +1,15 @@
 package com.example.bbs_test;
 
-public class BbsRepository {
 
+import java.util.List;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BbsRepository extends CrudRepository<Bbs, Long>{
+	
+	List<Bbs> findByOrderByIdDesc(Pageable pageable);
+	
 }
