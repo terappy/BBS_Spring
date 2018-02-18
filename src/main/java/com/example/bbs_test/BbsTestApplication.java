8727f1d1cbe7a90c60e2eb23e6c8f1660cddb62a
terappy <terakei818@gmail.com>
2018-02-18 14:22:31 +0900

    init project

---
+++ src/main/java/com/example/bbs_test/BbsTestApplication.java
@@ -0,0 +1,12 @@
+package com.example.bbs_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BbsTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BbsTestApplication.class, args);
+	}
+}
