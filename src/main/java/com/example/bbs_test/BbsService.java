d8fba1d6d99501c11d2663c54d0cea6122ab1fa5
terappy <terakei818@gmail.com>
2018-02-19 01:29:13 +0900

    edit BbsService.class

--- src/main/java/com/example/bbs_test/BbsService.java
+++ src/main/java/com/example/bbs_test/BbsService.java
@@ -1,5 +1,23 @@
 package com.example.bbs_test;
 
-public class BbsService {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class BbsService {
+	@Autowired
+	private BbsRepository repository;
+	
+	public List<Comment> getRecentComment(Integer n){
+		return repository.findByOrderByIdDesc(new PageRequest(0, n));
+	}
+	
+	@Transactional
+	public void save(Comment comment) {
+		repository.save(comment);
+	}
 }
